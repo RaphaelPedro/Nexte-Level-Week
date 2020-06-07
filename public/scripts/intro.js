b90c4ebe97bcbfec3c73896825e6f1dc210d0a36
Raphael Dias <raphaelpedrodias@outlook.com>
2020-06-07 12:40:08 -0300

    Enviando projeto semana Next Level Week

---
+++ public/scripts/intro.js
@@ -0,0 +1,73 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// var myvar // undefined
+// myvar = "He"
+// let tem o mesmo sentido de const
+// const myconst ="He"
+
+// document.write(myconst + myvar)
+
+// string
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso também é um string'
+// const s3 = `Isso é uma string também`
+
+// document.write(s2)
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objetos possuem propriedades e funcionalidades
+// const pessoa = {
+  // altura: "1,80m",
+  // idade: 24,
+  // solteiro: true,
+  // correr(){
+    // return "run Forest"
+  // }
+// }
+
+// document.write(pessoa.correr())
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue","green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[0])
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+// registrar função
+// function sayMyName(name) {
+  // document.write(name)
+// }
+
+// executar
+// sayMyName("Douglas")
+// sayMyName("Vanessa")
+// sayMyName("Kyam")
+
+// condicionais
+
+// const notaFinal = 4
+
+// if( notaFinal< 5 ) {
+  // document.write("Reprovado")
+  // caminho 1
+// } else {
+  // document.write("Aprovado")
+  // caminho 2
+// }
+
+// loop repetições
+
+// for (i = 0; i < 10; i++) {
+  // document.write(`<p>${i}</p>`)
+// }
